c1b1ad72efd6135d5e0ccd68a485bbcac095e02a
corn-snake <cornsnenk@gmail.com>
2025-02-07 20:22:20 +0000

    managed to figure out how to put two carousels in one column: put them in sized boxes

---
+++ example_11_carousel/android/app/src/main/kotlin/com/example/example_11_carousel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.example_11_carousel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ example_11_carousel/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("carousel view"),
+        centerTitle: true,
+        leading: Icon(Icons.account_tree_outlined),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(height:80,child: cvh()),
+            SizedBox(width: 5, height: 25,),
+            Container(height:160,child: cvv()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+CarouselView cvh() => CarouselView(
+  scrollDirection: Axis.horizontal,
+  itemExtent: 250, // Or any positive integers as long as the length of the array is 1.
+  children: List<Widget>.generate(10, (int index) {
+    return Center(child: Text('texto #$index'));
+  }),
+);
+
+CarouselView cvv() => CarouselView(
+  scrollDirection: Axis.vertical,
+  itemExtent: 80,
+  children: List<Widget>.generate(10, (int index) {
+    return Center(child: Text('$index° texto'));
+  }),
+);
